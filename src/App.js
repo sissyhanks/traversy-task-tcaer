9c411cb5a1aaefa8f3cdc9b1b66d567cc3d2cefa
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-13 14:13:59 -0400

    intro to props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     //you can only return a single parent element >> you can use <> </> as parent if you don't want a div wrapping around things
     <div className="container">
+      
       <Header />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-const Header = () => {
+//props >> can be passeed into the header function into the () and then in the <> </> of {props.propName} >> prop name as far as i know can be defined in the App js file>> <Header propName="print this i guess" OR do nothing in the App file and create default prop object below element (above ecport)/>
+
+
+const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{props.title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: "Task Tracker"
 }
 
 export default Header
