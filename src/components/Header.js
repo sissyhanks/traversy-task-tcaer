402bce816fafa9ca64dd60edf82e706dfac35ae0
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-13 14:19:04 -0400

    prop types

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 //props >> can be passeed into the header function into the () and then in the <> </> of {props.propName} >> prop name as far as i know can be defined in the App js file>> <Header propName="print this i guess" OR do nothing in the App file and create default prop object below element (above ecport)/>
 
+import PropTypes from 'prop-types'
+
+//prop types!! import them (above) and then set the type below (string // number // make code robust & cantch errors)
+
+
 
 const Header = (props) => {
   return (
@@ -11,6 +16,10 @@ const Header = (props) => {
 
 Header.defaultProps = {
   title: "Task Tracker"
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
