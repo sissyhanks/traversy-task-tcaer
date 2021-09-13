554b6980e0cd19612930d59fc66435890b26e77b
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-13 13:48:56 -0400

    Header element created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/Header'
+
 //everything on the page is coming from this function >> its JSX difference is like className instead of class things like that >> dynamically add JS expressions {{ ya know }}
 function App() {
   return (
     //you can only return a single parent element >> you can use <> </> as parent if you don't want a div wrapping around things
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header />
     </div>
   );
 }
