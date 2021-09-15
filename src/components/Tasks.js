db6b0bf473f2f2ac5e1109185e52e80cffeeb745
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-15 14:05:11 -0400

    create task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,4 @@
-
+import Task from './Task'
 //bring down the tasks prop form app here >> and will pass throu return 
 const Tasks = ( {tasks} ) => {
   // tasks brought inside of function and becomes part of the component state >> so don't do push.tasks because tasks is just a part ot the component state, setTasks is how to change the actual tasks setTasks ([...tasks {{{to spread across what is alreaty there}}} and then add new stuff or whatever]) >> but becasue we want to access tasks from other components we can't have them inside of this component, need ot make them global by putting it into APP so it can be taken down into other components as props (but really, probably do this dfwith api return or somethign like that).. movinmg the array in [3, 2, 1]
@@ -7,7 +7,8 @@ const Tasks = ( {tasks} ) => {
   return (
     <>
       {tasks.map((task) => (
-      <h3 key={task.id}>{task.text}</h3>
+        // after looping through the list above, this will wrap each individual task in the task component (from Task.js)
+      <Task key={task.id} task={task} />
       ))}
     </>
   )
