559457e2f904e81c91fa4dde212ef3d47a560272
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-15 13:34:33 -0400

    update component state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,34 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'dog show',
-    day: 'Feb 9th at 2:00am',
-    reminder: false,
-  },
-  {
-    id: 3,
-    text: 'test',
-    day: 'Feb 29th at 4:30pm',
-    reminder: true,
-  }
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+  // tasks brought inside of function and becomes part of the component state >> so don't do push.tasks because tasks is just a part ot the component state, setTasks is how to change the actual tasks setTasks ([...tasks {{{to spread across what is alreaty there}}} and then add new stuff or whatever])
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'one',
+      day: 'feb 1 at 1:00pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text: 'two',
+      day: 'feb 2 at 2:00pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text: 'three',
+      day: 'feb 3 at 3:00pm',
+      reminder: false,
+    },
+    {
+      id:4,
+      text: 'four',
+      day: 'feb 4 at 4:00pm',
+      reminder: true,
+    }
+  ])
+
   return (
     <>
       {tasks.map((task) => (
