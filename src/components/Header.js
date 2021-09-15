e1de85d076ba23bef8359c9923e2daf914841f98
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-15 12:40:11 -0400

    headder & button components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 //props >> can be passeed into the header function into the () and then in the <> </> of {props.propName} >> prop name as far as i know can be defined in the App js file>> <Header propName="print this i guess" OR do nothing in the App file and create default prop object below element (above ecport)/>
 
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 //prop types!! import them (above) and then set the type below (string // number // make code robust & cantch errors)
 
@@ -8,8 +9,9 @@ import PropTypes from 'prop-types'
 
 const Header = (props) => {
   return (
-    <header>
+    <header className='header'>
       <h1>{props.title}</h1>
+      <Button />
     </header>
   )
 }
