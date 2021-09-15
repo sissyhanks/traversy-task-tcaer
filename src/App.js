48d4aa64caed8206ed6c87b0649b3a823f59f1a2
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-15 13:42:38 -0400

    app level / global state set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+//userState was in tasks, but moving to APp to make the tasks array globally accessible 
+import { useState } from 'react'
 
 //everything on the page is coming from this function >> its JSX difference is like className instead of class things like that >> dynamically add JS expressions {{ ya know }}
 function App() {
+  //now tasks is part of our app level state
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'one',
+      day: 'feb 1 at 1:00pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text: 'two',
+      day: 'feb 2 at 2:00pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text: 'three',
+      day: 'feb 3 at 3:00pm',
+      reminder: false,
+    },
+    {
+      id:4,
+      text: 'four',
+      day: 'feb 4 at 4:00pm',
+      reminder: true,
+    }
+  ])
+
   return (
     //you can only return a single parent element >> you can use <> </> as parent if you don't want a div wrapping around things
     <div className="container">
       <Header />
-      <Tasks />
+      {/* tasks passed into Tasks as prop  */}
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,8 @@
-import { useState } from 'react'
 
-const Tasks = () => {
-  // tasks brought inside of function and becomes part of the component state >> so don't do push.tasks because tasks is just a part ot the component state, setTasks is how to change the actual tasks setTasks ([...tasks {{{to spread across what is alreaty there}}} and then add new stuff or whatever])
-  const [tasks, setTasks] = useState([
-    {
-      id:1,
-      text: 'one',
-      day: 'feb 1 at 1:00pm',
-      reminder: true,
-    },
-    {
-      id:2,
-      text: 'two',
-      day: 'feb 2 at 2:00pm',
-      reminder: true,
-    },
-    {
-      id:3,
-      text: 'three',
-      day: 'feb 3 at 3:00pm',
-      reminder: false,
-    },
-    {
-      id:4,
-      text: 'four',
-      day: 'feb 4 at 4:00pm',
-      reminder: true,
-    }
-  ])
+//bring down the tasks prop form app here >> and will pass throu return 
+const Tasks = ( {tasks} ) => {
+  // tasks brought inside of function and becomes part of the component state >> so don't do push.tasks because tasks is just a part ot the component state, setTasks is how to change the actual tasks setTasks ([...tasks {{{to spread across what is alreaty there}}} and then add new stuff or whatever]) >> but becasue we want to access tasks from other components we can't have them inside of this component, need ot make them global by putting it into APP so it can be taken down into other components as props (but really, probably do this dfwith api return or somethign like that).. movinmg the array in [3, 2, 1]
+
 
   return (
     <>
